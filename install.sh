047c4e2d88ba6c3845cf40a196aa8ab103fef032
Timothé BARTHELEMY <53118040+timothe-barthelemy@users.noreply.github.com>
2025-11-25 16:02:09 +0100

    fix /etc/hosts

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#grep apps /etc/hosts || sudo echo 127.0.0.1 apps >> /etc/hosts
 podman container rm -f apps.example.com
 podman network rm ebs12212
 podman volume rm ebs12212
@@ -14,10 +15,9 @@ tar-out /u01 - | gzip -1 >> u01.gz
 exit
 EOF
 rm -f Oracle-E-Business-Suite-12.2.12_VISION_INSTALL-disk001.vmdk Oracle-E-Business-Suite-12.2.12_VISION_INSTALL.ovf
-podman build -t ebs12212 .
+podman build --no-cache --layers=false -t ebs12212 .
 podman network create ebs12212
 podman volume create ebs12212
 podman volume import ebs12212 u01.gz
-podman container run -d -p 8000:8000 -p 1521:1521 -p 7001:7001 -p 7002:7002 -v ebs12212:/u01 --network ebs12212 --hostname apps.example.com --name apps.example.com ebs:12212 
-#grep apps /etc/hosts || sudo echo 127.0.0.1 apps >> /etc/hosts
 rm -f root.gz u01.gz 
+podman container run -d -p 8000:8000 -p 1521:1521 -p 7001:7001 -p 7002:7002 -v ebs12212:/u01 --network ebs12212 --hostname apps.example.com --name apps ebs12212 
